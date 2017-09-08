29503cf8f0ba990705104838b72c574e1d58c30b
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:04:04 +0530

    CONSOLE_OOP3

--- Program.cs
+++ Program.cs
@@ -3,89 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace assignment_2
-{
+namespace Console_oop3
+{        
     class Program
     {
+        static employee empobj = new employee(); //static is empobj variable (an instance) size foe emp obj os used to store pointer is 2 bytes //high frequrncy heap 
         static void Main(string[] args)
         {
-         
-        Console.WriteLine("Enter the customer details:");
-            
-        Console.WriteLine("Enter the customer id:");
-            int account_id=Convert.ToInt32(Console.ReadLine());
+            project p = new project();
+            employee.count = 10;
+            Console.WriteLine(employee.count);
+            employee obj = new employee();
+            obj.Employee_id = 1001;
+            obj.update();
+            obj.update();
+            Console.WriteLine(employee.count);
 
-            Console.WriteLine("Enter the customer Name:");
-            string customer_name = Console.ReadLine();
-
-            Console.WriteLine("Enter the customer dob (mm/dd/yyyy):");
-            DateTime customer_dob =Convert.ToDateTime(Console.ReadLine());
+            employee.count = 100;
+            Console.WriteLine(employee.count);
+           int i= employee.getdata();
+           Console.WriteLine(i);
+           obj.update();
+           obj.update();
+           obj.setproject(p);
+           employee obj2 = new employee();
+           p = null; // its not a memory leakage
+           obj = null; //memory leakage 
+           obj2 = null;//memory leakage 
            
-                Account obj = new Account(account_id,customer_name,customer_dob);
-
-       
-            int option;
-            do
-            {
-
-                Console.WriteLine("Enter the option 1.getbalance 2.withdraw 3.deposit 4.customer details: 5.exit");
-                option = Convert.ToInt32(Console.ReadLine());
-                switch (option)
-                {
-                    case 1:
-                        double balance = obj.getbalance();
-                        Console.WriteLine(" Available Balance:" + balance);
-                        break;
-                    case 2:
-
-                        Console.WriteLine("Enter the amount to be withdrawn");
-                        int amountw = Convert.ToInt32(Console.ReadLine());
-                        bool resultw = obj.withdraw(amountw);
-                        if (resultw)
-                        {
-
-                            Console.WriteLine(" Amount have been successfully withdrawn");
-                        }
-                        else
-                        {
-
-                            Console.WriteLine("Enter the correct amount to be withdrawn:");
-                        }
-                        break;
-
-                    case 3:
-
-                        Console.WriteLine("Enter the amount to be deposited");
-                        int amountd = Convert.ToInt32(Console.ReadLine());
-                        bool resultd = obj.deposit(amountd);
-                        if (resultd)
-                        {
-
-                            Console.WriteLine(" Amount have been successfully deposited");
-                        }
-                        else
-                        {
-
-                            Console.WriteLine("Enter the correct amount to be deposit:");
-                        }
-                        break;
-                    case 4:
-                        Console.WriteLine(" Customer details:");
-                        Console.WriteLine("Account id:" + obj.paccount_id);
-
-                        Console.WriteLine("Customer name:" + obj.pcustomer_name);
-
-                        Console.WriteLine("Customer DOB:" + obj.pcustomer_dob);
-                        
-
-                        Console.WriteLine("Account Balance:" + obj.paccount_balance);
-                        break;
-                    case 5:
-                        Console.WriteLine("THANK YOU");
-                        break;
-                }
-            } while (option != 5);
             Console.ReadLine();
-         }
+        }
     }
 }
---
+++ employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Console_oop3
+{
+    class employee
+    {
+        public int Employee_id;
+        public static int count;
+        public project empproject;
+        static employee()
+        {
+            count = 5;
+        }
+        public void setproject(project p)
+        {
+            empproject = p;
+        }
+
+
+        public void update()
+        {
+            employee.count++;
+
+        }
+        public static int getdata()  // in this function we cannot use employee id directly
+        {
+            return 100;
+        }
+     
+    }
+}
