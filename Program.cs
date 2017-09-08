a855e37cfb5634a3378d8b77731b8cb467323e4a
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:02:00 +0530

    CONSOLE_JAGGEDARRAY

--- Program.cs
+++ Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
+using System.Text;
 
-namespace practice
+namespace Console_jaggedarray
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            int[][] jaggedarray = new int[3][];
+            jaggedarray[0] = new int[2];
+            jaggedarray[1] = new int[3];
+            jaggedarray[2] = new int[1];
+            jaggedarray[0][0] = 25;
+            jaggedarray[0][1] = 30;
+            jaggedarray[1][0] = 12;
+
+            jaggedarray[1][1] = 31;
+            jaggedarray[1][2] = 55;
+            jaggedarray[2][0] = 322;
+            foreach (int[] array in jaggedarray)
+            {
+                foreach (int marks in array)
+                {
+                    Console.WriteLine(marks);
+                }
+            }
+            Console.ReadLine();
+
+
+
         }
     }
 }
