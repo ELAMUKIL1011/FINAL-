b89b2e2f69a87c745fbb0c02a1ee833f1e4686cb
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:06:29 +0530

    ASSIGNMENT_3.1

--- Program.cs
+++ Program.cs
@@ -3,51 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Consoleoverrid
+namespace assignment_3._1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enteremployee details:");
-            Console.WriteLine("enteremployee id:");
-            
-            int employeeid = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enteremployee name:");
-            string employeename = Console.ReadLine();
-            Console.WriteLine("enteremployee salaryperday:");
-
-            int employeesalaryperday = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enteremployee no of work days:");
-
-            int noofdays= Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(" enter the employee type noraml / contract");
-            string option = Console.ReadLine();
-            employee obj=null;
-            if (option == "normal")
+          string option1;  
+            int customerordercount=0;
+               int ordercount = 0;
+               int ordervalue=0;
+            do
             {
-                 obj = new employee(employeeid, employeename, employeesalaryperday, noofdays);
-            }
-     
-            else if (option == "contract")
-            {
-                 obj = new employee_contract(employeeid, employeename, employeesalaryperday, noofdays);
-            }
-            else {
+                Console.WriteLine("Cuustomer is there yes? else no");
+                option1 = Console.ReadLine();
+                if (option1 == "yes")
+                {
+                    string option;
+                 
+                    int itemid;
+                    int itemqty;
+                    int itemprice = 0;
+                    string customername;
 
-                Console.WriteLine("not valid");
-            }
-            if (obj != null)
-            {
-                string details = obj.getdetails();
-                Console.WriteLine(details);
-                obj.getwork();
-                int salary = obj.getsalary();
-                Console.WriteLine(salary);
-            }
+
+                    Console.WriteLine("enter the customer name");
+                    customername = Console.ReadLine();
+                    
+                    do
+                    {
+
+                        Console.WriteLine("enter yes to order / no to exit");
+                        option = Console.ReadLine();
+                        if (option == "yes")
+                        {
+
+                            Console.WriteLine("enter the item to be purchased");
+                            Console.WriteLine("1.phone,2.laptop,3.tab");
+                            itemid = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("enter the number of items to be purchased");
+                            itemqty = Convert.ToInt32(Console.ReadLine());
+
+                            switch (itemid)
+                            {
+                                case 1:
+                                    itemprice = 100;
+                                    break;
+                                case 2:
+                                    itemprice = 200;
+                                    break;
+                                case 3:
+                                    itemprice = 300;
+                                    break;
+
+                            }
+                            // Console.WriteLine("your order id for the purchased");
+                            order obj = new order(itemid, itemqty, itemprice, customername);
+                            ordercount = order.GetTotalOrderCount();
+                            ordervalue = obj.pordervalue;
+                        }
+                        else
+                        {
+                            Console.WriteLine("thank you !!!!Visit again");
+                        }
+
+                    } while (option!= "no");
+                    Console.WriteLine("total Number of order you made");
+                    Console.WriteLine("order count" +ordercount);
+                    Console.WriteLine(ordervalue);
+                    Console.WriteLine(order.pcustomercount);
+        
+                }
+                else
+                {
+                    Console.WriteLine(" over");
+                }
+
+            } while (option1 != "no");
+            Console.WriteLine("total orders made by all customers");
+            Console.WriteLine(order.GetTotalOrderCount());
             Console.ReadLine();
-        
-        
-}
+        }
     }
 }
---
+++ order.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assignment_3._1
+{
+    class order
+    {
+        private  int orderid;
+        private static int customercount;
+        public static int pcustomercount
+        {
+            get
+            {
+                return customercount;
+            }
+        }
+
+        private int itemid;
+        private int itemqty;
+        private int itemprice;
+        private static int count;
+        private string customername;
+        private static string customernamecheck="";
+        public int porderid
+        {
+            get
+            {
+                return orderid;
+            }
+        }
+        public int pordervalue
+        {
+            get
+            {
+                return itemprice * itemqty;
+            }
+        }
+        public order(int itemid, int itemqty, int itemprice, string customername)
+        {
+            count++;
+            
+            this.orderid = count;
+            this.itemid = itemid;
+            this.itemqty = itemqty;
+            this.itemprice = itemprice;
+            this.customername = customername;
+            if (customernamecheck != customername)
+            {
+                customernamecheck = customername;
+                customercount = 1;
+            }
+            else
+            {
+                customercount++;
+            }
+
+
+         //   Console.WriteLine("caled constructor");
+        }
+        public static  int GetTotalOrderCount()
+        {
+            //Console.WriteLine("before" + orderid);
+           
+            // Console.WriteLine("before" + orderid);
+             return count;
+        }
+
+    }
+}
