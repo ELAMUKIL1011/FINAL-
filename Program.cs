660fe80734863d9be932798f512c559ea227a628
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:07:43 +0530

    ASSIGN_3.3

--- Program.cs
+++ Program.cs
@@ -3,86 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace assignment_3._1
+namespace assign3._3
 {
     class Program
     {
         static void Main(string[] args)
         {
-          string option1;  
-            int customerordercount=0;
-               int ordercount = 0;
-               int ordervalue=0;
-            do
+            
+            Console.WriteLine("enter order details");
+            Console.WriteLine("enter customer id");
+            int customerid = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter customer name");
+            string customername = Console.ReadLine();
+            Console.WriteLine("enter item price");
+            int itemprice = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter item qty");
+            int itemqty = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" enter order or overseas");
+            string option = Console.ReadLine();
+            order obj=null;
+            if(option=="order")
             {
-                Console.WriteLine("Cuustomer is there yes? else no");
-                option1 = Console.ReadLine();
-                if (option1 == "yes")
+       obj= new order(customerid, customername, itemqty, itemprice);
+            }
+                else if (option=="overseas")
                 {
-                    string option;
-                 
-                    int itemid;
-                    int itemqty;
-                    int itemprice = 0;
-                    string customername;
-
-
-                    Console.WriteLine("enter the customer name");
-                    customername = Console.ReadLine();
-                    
-                    do
-                    {
-
-                        Console.WriteLine("enter yes to order / no to exit");
-                        option = Console.ReadLine();
-                        if (option == "yes")
-                        {
-
-                            Console.WriteLine("enter the item to be purchased");
-                            Console.WriteLine("1.phone,2.laptop,3.tab");
-                            itemid = Convert.ToInt32(Console.ReadLine());
-                            Console.WriteLine("enter the number of items to be purchased");
-                            itemqty = Convert.ToInt32(Console.ReadLine());
-
-                            switch (itemid)
-                            {
-                                case 1:
-                                    itemprice = 100;
-                                    break;
-                                case 2:
-                                    itemprice = 200;
-                                    break;
-                                case 3:
-                                    itemprice = 300;
-                                    break;
-
-                            }
-                            // Console.WriteLine("your order id for the purchased");
-                            order obj = new order(itemid, itemqty, itemprice, customername);
-                            ordercount = order.GetTotalOrderCount();
-                            ordervalue = obj.pordervalue;
-                        }
-                        else
-                        {
-                            Console.WriteLine("thank you !!!!Visit again");
-                        }
-
-                    } while (option!= "no");
-                    Console.WriteLine("total Number of order you made");
-                    Console.WriteLine("order count" +ordercount);
-                    Console.WriteLine(ordervalue);
-                    Console.WriteLine(order.pcustomercount);
-        
+                    obj=new order_overseas(customerid, customername, itemqty, itemprice);
                 }
-                else
-                {
-                    Console.WriteLine(" over");
-                }
+                
+           
+            double result = obj.getordervalue();
 
-            } while (option1 != "no");
-            Console.WriteLine("total orders made by all customers");
-            Console.WriteLine(order.GetTotalOrderCount());
+            Console.WriteLine(obj.pcustomerid + " " + obj.pcustomername + " " + result);
+           
             Console.ReadLine();
+         
         }
+           
     }
 }
--- order.cs
+++ order.cs
@@ -3,69 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace assignment_3._1
+namespace assign3._3
 {
     class order
     {
-        private  int orderid;
-        private static int customercount;
-        public static int pcustomercount
+        protected int customerid;
+        protected string customername;
+        protected int itemqty;
+        protected int itemprice;
+        public int pcustomerid
         {
-            get
-            {
-                return customercount;
-            }
-        }
-
-        private int itemid;
-        private int itemqty;
-        private int itemprice;
-        private static int count;
-        private string customername;
-        private static string customernamecheck="";
-        public int porderid
-        {
-            get
-            {
-                return orderid;
-            }
-        }
-        public int pordervalue
-        {
-            get
-            {
-                return itemprice * itemqty;
-            }
-        }
-        public order(int itemid, int itemqty, int itemprice, string customername)
-        {
-            count++;
+            get {
+                return customerid;
             
-            this.orderid = count;
-            this.itemid = itemid;
-            this.itemqty = itemqty;
-            this.itemprice = itemprice;
-            this.customername = customername;
-            if (customernamecheck != customername)
-            {
-                customernamecheck = customername;
-                customercount = 1;
             }
-            else
-            {
-                customercount++;
-            }
-
-
-         //   Console.WriteLine("caled constructor");
         }
-        public static  int GetTotalOrderCount()
+        public string pcustomername
         {
-            //Console.WriteLine("before" + orderid);
-           
-            // Console.WriteLine("before" + orderid);
-             return count;
+            get
+            {
+                return customername;
+
+            }
         }
+        
+        public order(int customerid, string customername, int itemqty, int itemprice)
+        {
+            this.customerid = customerid;
+            this.customername = customername;
+            this.itemprice = itemprice;
+            this.itemqty = itemqty;
+
+
+        }
+        public virtual double getordervalue()
+        {
+
+            return itemqty*itemprice;
+        }
+        
 
     }
 }
