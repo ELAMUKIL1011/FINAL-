e7ed054e0aefc835fb51ae2e1cf4776660c35130
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:08:19 +0530

    ASSIGNMENT_3.2

--- Program.cs
+++ Program.cs
@@ -3,42 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace assign3._3
+namespace assignment3._2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("enter order details");
-            Console.WriteLine("enter customer id");
-            int customerid = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter customer name");
-            string customername = Console.ReadLine();
-            Console.WriteLine("enter item price");
-            int itemprice = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("enter item qty");
-            int itemqty = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(" enter order or overseas");
-            string option = Console.ReadLine();
-            order obj=null;
-            if(option=="order")
-            {
-       obj= new order(customerid, customername, itemqty, itemprice);
-            }
-                else if (option=="overseas")
-                {
-                    obj=new order_overseas(customerid, customername, itemqty, itemprice);
-                }
-                
-           
-            double result = obj.getordervalue();
 
-            Console.WriteLine(obj.pcustomerid + " " + obj.pcustomername + " " + result);
-           
+            manager m1 = manager.getobject();
+            manager m2 = manager.getobject();
+            if (m1 == m2)
+            {
+                Console.WriteLine("singleton");
+            }
             Console.ReadLine();
-         
+
         }
-           
     }
 }
---
+++ manager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assignment3._2
+{
+    class manager
+    {
+        private int managerid;
+        private string managername;
+        public int pmanagerid
+        {
+            get
+            {
+                return managerid;
+            }
+        }
+        public string pmanagername
+        {
+            get
+            {
+                return managername;
+            }
+        }
+
+        private manager()
+        {
+        }
+        static manager m1;
+        public static manager getobject()
+        {
+            if (m1 == null)
+            {
+                m1 = new manager();
+                m1.managerid = 1;
+                m1.managername = "Aishwarya";
+                return manager.m1;
+            }
+
+
+
+
+            return manager.m1;
+
+
+
+        }
+    }
+}
