d00bfb1c2f8a3ad4420d9d480c4c6e2451919dc8
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:03:34 +0530

    ASSIGNMENT_2

---
+++ Account.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace assignment_2
+{
+    class Account
+    {
+        private int account_id;
+        private string customer_name;
+        private DateTime customer_dob;
+        private double account_balance;
+
+        public Account(int account_id,string customer_name,DateTime customer_dob)
+        {
+            this.account_id=account_id;
+            this.customer_name=customer_name;
+            this.customer_dob=customer_dob;
+        }
+        public int paccount_id
+        {
+            get
+            {
+                return account_id;
+            }
+        }
+        public string pcustomer_name
+        {
+            get
+            {
+                return customer_name;
+            }
+        }
+        public DateTime pcustomer_dob
+        {
+            get
+            {
+                return customer_dob;
+            }
+        }
+        public double paccount_balance
+        {
+            get
+            {
+                return account_balance;
+            }
+           
+        }
+        public double getbalance()
+        {
+            return account_balance;
+        }
+
+        public bool withdraw(int amountw)
+        {
+            if (amountw > account_balance)
+            {
+                return false;
+            }
+            else
+            {
+                account_balance = account_balance - amountw;
+                return true;
+            }
+        }
+        public bool deposit(int amountd)
+        {
+            if (amountd > 0)
+            {
+                account_balance = account_balance + amountd;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+     }
+}
--- Program.cs
+++ Program.cs
@@ -3,61 +3,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Console_oop2
+namespace assignment_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Student obj = new Student(100, "vifhh", 60);
-            Console.WriteLine(obj.Pstudentid);
-            Console.WriteLine(obj.Pstudentname);
-            Console.WriteLine(obj.Pstudentmarks);
+         
+        Console.WriteLine("Enter the customer details:");
+            
+        Console.WriteLine("Enter the customer id:");
+            int account_id=Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine(obj.Pstudentstatus);
-            obj.Pstudentname = Console.ReadLine()   ;
-            obj.Pstudentmarks = 1000;
-            Console.WriteLine(obj.Pstudentmarks);
-            Console.WriteLine(obj.Pstudentstatus);
-            Console.WriteLine(obj.Pstudentname);
-            obj.Pstudentmarks = 75;
-            Console.WriteLine(obj.Pstudentmarks);
-            Console.WriteLine(obj.Pstudentstatus);
+            Console.WriteLine("Enter the customer Name:");
+            string customer_name = Console.ReadLine();
 
-
-
-
-
-
-         /*   customer c = new customer();
-            c.getdata();
-            Test obj = new Test(1000,"abc");
-            Customernameupdatestatus status = obj.update(100, "");
-            if (status==Customernameupdatestatus.Updated)
-            {
-                Console.WriteLine("yes updated");
-            }
+            Console.WriteLine("Enter the customer dob (mm/dd/yyyy):");
+            DateTime customer_dob =Convert.ToDateTime(Console.ReadLine());
            
-else if(status==Customernameupdatestatus.invalidname)
+                Account obj = new Account(account_id,customer_name,customer_dob);
+
+       
+            int option;
+            do
             {
-                Console.WriteLine("Customer name is invalid");
 
-            }
-            else if(status==Customernameupdatestatus.Invalidcustomerid)
-            {
-                Console.WriteLine(" customer id is invalid");
-            }
+                Console.WriteLine("Enter the option 1.getbalance 2.withdraw 3.deposit 4.customer details: 5.exit");
+                option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        double balance = obj.getbalance();
+                        Console.WriteLine(" Available Balance:" + balance);
+                        break;
+                    case 2:
 
+                        Console.WriteLine("Enter the amount to be withdrawn");
+                        int amountw = Convert.ToInt32(Console.ReadLine());
+                        bool resultw = obj.withdraw(amountw);
+                        if (resultw)
+                        {
 
-            int salary = obj.getsalary(10000, 31); //normal way
-            Console.WriteLine(salary);
-            salary = obj.getsalary(10000); //oneoptional parameter 
-            Console.WriteLine(salary);
-            salary = obj.getsalary(no:31,perdayssalary: 10000); //named parameter 
-            Console.WriteLine(salary);*/
+                            Console.WriteLine(" Amount have been successfully withdrawn");
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Enter the correct amount to be withdrawn:");
+                        }
+                        break;
+
+                    case 3:
+
+                        Console.WriteLine("Enter the amount to be deposited");
+                        int amountd = Convert.ToInt32(Console.ReadLine());
+                        bool resultd = obj.deposit(amountd);
+                        if (resultd)
+                        {
+
+                            Console.WriteLine(" Amount have been successfully deposited");
+                        }
+                        else
+                        {
+
+                            Console.WriteLine("Enter the correct amount to be deposit:");
+                        }
+                        break;
+                    case 4:
+                        Console.WriteLine(" Customer details:");
+                        Console.WriteLine("Account id:" + obj.paccount_id);
+
+                        Console.WriteLine("Customer name:" + obj.pcustomer_name);
+
+                        Console.WriteLine("Customer DOB:" + obj.pcustomer_dob);
+                        
+
+                        Console.WriteLine("Account Balance:" + obj.paccount_balance);
+                        break;
+                    case 5:
+                        Console.WriteLine("THANK YOU");
+                        break;
+                }
+            } while (option != 5);
             Console.ReadLine();
-
-
-        }
+         }
     }
 }
