58b61ab2aea7bdcc686a7031fdab98a5f97c015d
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:20:09 +0530

    DELEGATE

--- Program.cs
+++ Program.cs
@@ -1,21 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Windows.Forms;
+using System.Text;
 
-namespace Win_assembly
+namespace console_delegate
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+
+            test t = new test();
+            t.run();
+            Console.ReadLine();
+
+
+
         }
     }
 }
--- test.cs
+++ test.cs
@@ -3,17 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Console_generics
+namespace console_delegate
 {
-    class test<x>
+    class test
     {
-        public x getdata(x i)
+        public delegate void del(string str);
+        public delegate void del1(int i);
+        public delegate void delgeneric<t>(t str); // delegate generics 
+
+        public void getdata(string msg)
         {
-            return i; 
+
+            Console.WriteLine("getdata" + msg);
+        
         }
-        public T Genericfun<T>(T obj)
+
+        public void setdata(string str)
+    {
+    Console.WriteLine("set data "+str);
+    }
+
+
+        public void run()
         {
-            return obj;
+            int x = 100;
+            del obj = new del(this.getdata);
+            obj += new del(this.setdata);// adding address
+            //obj -= new del(this.setdata); // removing address
+            obj += delegate(string str)
+            {
+
+                Console.WriteLine("anonymous func  " + str  + "  "+ x);
+
+            };
+            obj += new del((s) => Console.WriteLine("lambda expression " + s));
+            obj += new del((s) =>
+            {
+                Console.WriteLine("lambda expression ");
+                Console.WriteLine("with multiple line " + s);
+            });
+                
+                
+                
+            obj("hello"); // calls getdata  // single cast (one address only)
+
+
+        
         }
     }
 }
