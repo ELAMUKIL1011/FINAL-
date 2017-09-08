9f3ac4a0a4fa176aecfcf3ccbf4babea042efdcc
ELAMUKIL1011 <31727888+ELAMUKIL1011@users.noreply.github.com>
2017-09-08 20:18:33 +0530

    SUM_LIB

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace sum_library
+{
+    public class clac
+    {
+        public int getsum(int num1, int num2) // must be public for dll 
+        {
+
+
+            return num1 + num2+200;
+
+        
+        }
+
+    }
+}
